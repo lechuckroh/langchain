60f71e3c37f40fa31af21cdfe6af5e0b47b895e3
Lechuck Roh <lechuckroh@gmail.com>
2024-12-29 16:09:06 +0900

    add caching example

--- src/llama_chat.py
+++ src/llama_chat.py
@@ -1,15 +1,22 @@
-from langchain_core.prompts import ChatPromptTemplate
+import time
+
+from langchain.globals import set_llm_cache
+from langchain.schema import HumanMessage
+from langchain_core.caches import InMemoryCache
 from langchain_ollama import ChatOllama
+
+set_llm_cache(InMemoryCache())
 
 chat = ChatOllama(model="llama3.2-korean")
 
-prompt = ChatPromptTemplate.from_messages([
-    ("human", "{input}"),
-])
+start = time.time()
+result = chat.invoke([HumanMessage(content="안녕하세요!")])
+end = time.time()
+print(result.content)
+print(f"Elapsed time: {end - start}s")
 
-chain = prompt | chat
-
-ai_msg = chain.invoke({
-    "input": "iPhone14 출시일을 알려줘",
-})
-print(ai_msg.content)
+start = time.time()
+result = chat.invoke([HumanMessage(content="안녕하세요!")])
+end = time.time()
+print(result.content)
+print(f"Elapsed time: {end - start}s")
